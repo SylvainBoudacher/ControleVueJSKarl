b4afcb7cf9f3fe2d62f55e98ae460cf2943c296c
Sulyk <sylvain.boudacher@gmail.com>
2022-12-16 17:19:24 +0100

    add Formik

---
+++ ControleVueJsKarl/src/components/Formik.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  initialValues: {
+    type: Object,
+    required: true,
+  },
+  validate: {
+    type: Function,
+    required: true,
+  },
+  onSubmit: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<template></template>
+
+<style></style>
